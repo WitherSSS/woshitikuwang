4760be238671aa8a954606f00aecb83d8049e7f2
フェリセッタ <68819438+WitherSSS@users.noreply.github.com>
2023-12-12 23:18:20 +0800

    Update public.js

--- js/public.js
+++ js/public.js
@@ -2,6 +2,6 @@ const JSONList = [
     { id: "000001", name: '廉洁知识竞赛——初赛', describe: '廉洁知识竞赛——初赛', file: "廉洁初赛.json" },
     {id: "000002", name: '安规题库', describe: '2023年新进员工《安规》考试题库（变电专业）', file: "安规（变电）.json"   },
     {id: "000004", name: '安全等级一级', describe: '安全等级一级', file: "安全等级.json"   },
-    {id: "000003", name: '传统知识竞赛', describe: '传统知识竞赛', file: "传统知识竞赛.json"   }
-    
+    {id: "000003", name: '传统知识竞赛', describe: '传统知识竞赛', file: "传统知识竞赛.json"   },
+    {id: "000005", name: '高处作业', describe: '特种高处作业2022版题库', file: "高处.json"   },
 ]
