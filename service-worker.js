3146f4755b585361217b9625ce33a686900fd91e
フェリセッタ <zhuw160@gmail.com>
2024-11-02 19:47:45 +0800

    add

--- service-worker.js
+++ service-worker.js
@@ -21,6 +21,7 @@ const urlsToCache = [
   '/json/安全等级.json',
   '/json/高处.json',
   '/json/继电保护2024.json',
+  '/json/20241.11.json',
   '/images/icon-192.png',
   '/images/icon-128.png',
   '/images/icon-512.png'
