5c6c6805c3b606d18a32631c88ffee986e12da29
フェリセッタ <68819438+WitherSSS@users.noreply.github.com>
2024-12-17 11:09:24 +0800

    更新 public.js

--- js/public.js
+++ js/public.js
@@ -6,4 +6,5 @@ const JSONList = [
     {id: "000005", name: '高处作业', describe: '特种高处作业2022版题库', file: "高处.json"   },
     {id: "000006", name: '继电保护员', describe: '（浙江）2024高级工及以下等级（继电保护员）专业知识考试自测联系', file: "继电保护2024.json"   },
     {id: "000007", name: '季度考核', describe: '2024.11考核', file: "2024.11.json"   },
+{id: "000008", name: '八、二次回路', describe: '2024.11考核', file: "8二次回路.json"   },
 ]
